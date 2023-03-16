ea4e2848015d3ccf156eca732a37c203e0b23290
subhamyadav01 <90240318+subhamyadav01@users.noreply.github.com>
2023-03-16 22:36:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// var searchIcon=document.querySelector(".menu-icon");
+// var sidebar=document.querySelector(".sidebar");
+
+// menuIcon.onclick=function(){
+//     sidebar.classList.toggle("small-sidebar");
+// }
